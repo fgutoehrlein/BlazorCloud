22ff1c15e8249020f43c164e4d4c5b94e22e2db9
Felix Gutoehrlein <fgutoeh@emea.corpdir.net>
2021-12-27 13:27:31 +0100

    Some changes :P

--- BlazorCloud/Controller/DataController.cs
+++ BlazorCloud/Controller/DataController.cs
@@ -36,7 +36,7 @@ namespace BlazorCloud.Controller
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<List<FileBase>>> GetFileNamesAsync()
+        public async Task<ActionResult<List<FileBase>>> GetFileNamesAsync(string path)
         {
             if (!(await _basicAuthorization.BasicAuthIsValid(HttpContext)))
             {
@@ -45,7 +45,7 @@ namespace BlazorCloud.Controller
 
             try
             {
-                var fileNames = await _fileAndDirectoryService.GetListOfFilesFromPathAsync("");
+                var fileNames = await _fileAndDirectoryService.GetListOfFilesFromPathAsync(path);
                 return Ok(fileNames);
             }
             catch (Exception e)
